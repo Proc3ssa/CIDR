0d834e3b348f5d64112a58da0ce3cbfcc912e0a9
Faisal <pros3sa@gmail.com>
2024-03-29 11:16:32 +0000

    added css

--- src/components/input.js
+++ src/components/input.js
@@ -5,12 +5,12 @@ const Inputs = () => {
     return <div className="general">
         <h1>C.I.D.R. Blocks Calculator</h1>
         <div className="inputs">
-            <input type="number"/>
-            <input type="number"/>
-            <input type="number"/>
-            <input type="number"/>
+            <input type="number" placeholder="172"/>.
+            <input type="number" placeholder="20"/>.
+            <input type="number" placeholder="10"/>.
+            <input type="number" placeholder="1"/>
             <span>/</span>
-            <input type="number"/>
+            <input type="number" placeholder="16"/>
         </div>
 
         <div className="button"><button>Calculate</button></div>
